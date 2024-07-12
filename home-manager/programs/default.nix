048a30bdac6d20ec0a3915ad5601d590517ba8b9
techyporcupine <git@cb-tech.me>
2024-07-12 19:27:50 -0400

    move zsh

--- home-manager/programs/default.nix
+++ home-manager/programs/default.nix
@@ -3,7 +3,6 @@
     ./waybar
     ./rofi
     ./swaync
-    ./zsh
     ./gtk
     ./mangohud
     ./qt
--- nixos/misc-system.nix
+++ nixos/misc-system.nix
@@ -77,7 +77,60 @@
         };
       };
     };
-    
+
+    tp.hm.programs = {
+      # Zsh configuration
+      zsh = {
+        enable = true;
+        oh-my-zsh = {
+          enable = true;
+          plugins = [
+            "git"
+            "sudo"
+          ];
+        };
+        # Defined aliases to be used inside of the shell 
+        shellAliases = {
+          c = "clear";
+        };
+        autosuggestion.enable = true;
+        enableCompletion = true;
+        syntaxHighlighting.enable = true;
+      };
+      # Configuration for starship, my zsh theme
+      starship = {
+        enable = true;
+        enableZshIntegration = true;
+        settings = {
+          format = "$directory$character$git_branch$git_status";
+          right_format = "$status$cmd_duration";
+          character = {
+            success_symbol = "[❯](blue)";
+            error_symbol = "[❯](red)";
+          };
+          status = {
+            disabled = false;
+            format = "[$symbol]($style)";
+            symbol = "[✘ ](red)";
+            success_symbol = "[✔ ](green)";
+          };
+          git_branch = {
+            format = "[$branch]($style) ";
+            style = "bold green";
+          };
+          directory = {
+            style = "blue";
+            truncation_length = 1;
+            truncation_symbol = "";
+            fish_style_pwd_dir_length = 1;
+          };
+          cmd_duration = {
+            min_time = 0;        
+          };
+        };
+      };
+    };
+
     # SOUND CONFIG
     hardware.pulseaudio.enable = false;
     security.rtkit.enable = true;
