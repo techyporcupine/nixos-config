2892dab425ac5b8343cbce7b6d719b9842ebc739
techyporcupine <git@cb-tech.me>
2024-07-12 19:22:55 -0400

    move SSH config that was in hm

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -5,46 +5,4 @@
   imports = [
     ./programs
   ];
-
-  # Enable SSH agest
-  services = {
-    ssh-agent.enable = true;
-  };
-
-  programs = {
-    # SSH home config
-    ssh = {
-      enable = true;
-      addKeysToAgent = "yes";
-      # Config for clients you can ssh to without all their info.
-      matchBlocks = {
-        "helium" = {
-          forwardAgent = true;
-          hostname = "10.0.0.133";
-          setEnv = { TERM = "kitty"; };
-        };
-        "nixserve" = {
-          forwardAgent = true;
-          hostname = "10.0.0.5";
-          setEnv = { TERM = "kitty"; };
-        };
-        "printers" = {
-          forwardAgent = true;
-          user = "printers";
-          hostname = "10.0.0.30";
-          setEnv = { TERM = "kitty"; };
-        };
-        "switch" = {
-          hostname = "10.0.0.4";
-          user = "admin";
-          extraOptions = {
-            PubkeyAcceptedAlgorithms = "+ssh-rsa";
-            HostkeyAlgorithms = "+ssh-rsa";
-            Ciphers = "aes128-ctr";
-            KexAlgorithms = "+diffie-hellman-group1-sha1";
-          };
-        };
-      };
-    };
-  };
 }
--- nixos/misc-system.nix
+++ nixos/misc-system.nix
@@ -39,7 +39,45 @@
       settings.KbdInteractiveAuthentication = false;
     };
     programs.ssh.startAgent = true;
-
+    tp.hm.services = {
+      ssh-agent.enable = true;
+    };
+    tp.hm.programs = {
+      ssh = {
+        enable = true;
+        addKeysToAgent = "yes";
+        # Config for clients you can ssh to without all their info.
+        matchBlocks = {
+          "helium" = {
+            forwardAgent = true;
+            hostname = "10.0.0.133";
+            setEnv = { TERM = "kitty"; };
+          };
+          "nixserve" = {
+            forwardAgent = true;
+            hostname = "10.0.0.5";
+            setEnv = { TERM = "kitty"; };
+          };
+          "printers" = {
+            forwardAgent = true;
+            user = "printers";
+            hostname = "10.0.0.30";
+            setEnv = { TERM = "kitty"; };
+          };
+          "switch" = {
+            hostname = "10.0.0.4";
+            user = "admin";
+            extraOptions = {
+              PubkeyAcceptedAlgorithms = "+ssh-rsa";
+              HostkeyAlgorithms = "+ssh-rsa";
+              Ciphers = "aes128-ctr";
+              KexAlgorithms = "+diffie-hellman-group1-sha1";
+            };
+          };
+        };
+      };
+    };
+    
     # SOUND CONFIG
     hardware.pulseaudio.enable = false;
     security.rtkit.enable = true;
