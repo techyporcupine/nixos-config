c10ac7f7d9ce60ec6da8d659b3d1aeb8b913fc85
techyporcupine <git@cb-tech.me>
2024-07-15 20:24:25 -0400

    add in config for mc server (works!)

--- nixos/minecraft.nix
+++ nixos/minecraft.nix
@@ -4,25 +4,27 @@
     minecraft-server.broccoli-bloc = lib.mkEnableOption "Config for Broccoli-bloc";
   };
 
+  imports = [ inputs.nix-minecraft.nixosModules.minecraft-servers ];
+
   config = lib.mkIf cfg.minecraft-server.enable {
-    imports = [ inputs.nix-minecraft.nixosModules.minecraft-servers ];
     nixpkgs.overlays = [ inputs.nix-minecraft.overlay ];
 
-    services.minecraft-servers.servers = {
+    services.minecraft-servers = {
       enable = true;
       eula = true;
-      broccoli-bloc = lib.mkIf cfg.minecraft-server.broccoli-bloc {
+      servers.broccoli-bloc = lib.mkIf cfg.minecraft-server.broccoli-bloc {
         enable = true;
         autoStart = true;
         openFirewall = true;
-        package = pkgs.paperServers.paper-1_21_66;
+        jvmOpts = "-Xms512M -Xmx8192M";
+        package = pkgs.paperServers.paper-1_21;
         serverProperties = {
           server-port = 25565;
           difficulty = "normal";
           gamemode = "survival";
           max-players = 20;
           motd = "Broccoli-Bloc Minecraft Server";
-          white-list = false; # TODO: ADJUST THIS WHEN PUBLIC
+          white-list = true;
         };
       };
     };
