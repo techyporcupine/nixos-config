68bc88c096835dd53d0bf6644e021f2b28869283
techyporcupine <git@cb-tech.me>
2024-07-21 13:40:20 -0400

    backups (NOT WORKING)

---
+++ nixos/backups.nix
@@ -0,0 +1,49 @@
+{pkgs, config, lib, inputs, ... }: let cfg = config.tp.backups; in {
+  options.tp.backups = {
+    enable = lib.mkEnableOption "Enable Restic backups";
+  };
+
+  config = lib.mkIf cfg.enable {
+    restic = {
+      backups = {
+        remotebackup-small = {
+          passwordFile = ;
+          initialize = true;
+          extraOptions = [
+            "sftp.command='ssh bowman4@100.64.0.6 -i /home/${config.tp.username}/.ssh/id_ed25519 -s sftp'"
+          ];
+          paths = [
+            "/srv/minecraft/"
+          ];
+          repository = "sftp:bowman4@100.64.0.6:/mnt/PiBackup/ResticBackups/MinecraftBackup";
+          timerConfig = {
+            OnCalendar = "daily";
+            Persistent = true;
+            RandomizedDelaySec = "10min";
+          };
+        };
+      };
+    };
+    systemd = {
+      timers = {
+        "restic-remotebackup-small-remove" = {
+          wantedBy = [ "timers.target" ];
+          timerConfig = {
+            OnCalendar = "daily";
+            Persistent = true; 
+            Unit = "restic-remotebackup-small-remove.service";
+          };
+        };
+      };
+      services = {
+        "restic-remotebackup-small-remove" = {
+          serviceConfig = {
+            ExecStart = ''/run/current-system/sw/bin/restic-remotebackup-small forget --keep-daily 7 --keep-weekly 26 --prune ''; 
+            Type = "oneshot";
+            User = "root";
+          };
+        };
+      };
+    };
+  };
+}
