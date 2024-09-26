35faace7b907d4694ddb0bfde2310c74d20bb6b6
techyporcupine <git@cb-tech.me>
2024-09-25 21:04:41 -0400

    sway improvements

--- nixos/graphics/sway/default.nix
+++ nixos/graphics/sway/default.nix
@@ -32,45 +32,58 @@
           # Use kitty as default terminal
           terminal = "kitty"; 
           startup = [
-            # Launch Firefox on start
+            # Launch some applications on start
             {command = "firefox";}
             {command = "hyprpaper";}
             {command = "hypridle";}
             {command = "swaync";}
             {command = "wl-paste --watch cliphist store";}
           ];
+          # Set command used to launch dmenu
           menu = "zsh -c 'rofi -show drun'";
+          # Set status bar used
           bars = [
             {command = "waybar";}
           ];
+          # Window related settings
           window = {
             border = 2;
           };
+          # Set the colors for focused windows
           colors.focused = {
-            background = "#285577";
+            background = "#89dceb";
             border = "#89dceb";
             childBorder = "#89dceb";
             indicator = "#89dceb";
-            text = "#ffffff";
+            text = "#000000";
           };
+          # Set window gaps
           gaps = {
             inner = 6;
           };
+          # Input device configuration: change touchpad interface for these to apply on other devices
           input = {
             "2362:628:PIXA3854:00_093A:0274_Touchpad" = {
               dwt = "disabled";
               tap = "enabled";
               middle_emulation = "enabled";
+              pointer_accel = "0.25";
             };
           };
+          # Fun keybindings
           keybindings = {
             "${modifier}+C" = "kill";
             "${modifier}+R" = "exec ${menu}";
             "${modifier}+V" = "exec cliphist list | rofi -dmenu | cliphist decode | wl-copy";
             "${modifier}+M" = "exec swaynag -t warning -m 'You pressed the exit shortcut. Do you really want to exit sway? This will end your Wayland session.' -b 'Yes, exit sway' 'swaymsg exit'";
             "${modifier}+N" = "exec swaync-client -t -sw";
+            "${modifier}+L" = "exec loginctl lock-session";
+            "${modifier}+W" = "floating toggle";
             "Print" = "exec grimshot copy area";
-
+            "XF86MonBrightnessUp" = "exec brightnessctl -s s +5%";
+            "XF86MonBrightnessDown" = "exec brightnessctl -s s 5%-";
+            "XF86AudioRaiseVolume" = "exec wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+ --limit 1";
+            "XF86AudioLowerVolume" = "exec wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%- --limit 1";
 
 
             # Workspace mods
@@ -93,12 +106,14 @@
             "${modifier}+Shift+8" = "move container to workspace number 8";
             "${modifier}+Shift+9" = "move container to workspace number 9";
           };
-        };
+        }; 
+        # Extra config stuff that didn't have options in Nix
         extraConfig = ''
           default_border pixel
           corner_radius 10
           bindgesture swipe:right workspace prev
           bindgesture swipe:left workspace next
+          seat seat0 xcursor_theme catppuccin-mocha-green-cursors 24
         '';
       };
     };
