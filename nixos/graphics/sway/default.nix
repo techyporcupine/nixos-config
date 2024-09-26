f714d828823b0deb37aeb2ea6b0606b6fb50d9ba
techyporcupine <git@cb-tech.me>
2024-09-26 09:41:02 -0400

    change window movement bindings to be more like hyprland

--- nixos/graphics/sway/default.nix
+++ nixos/graphics/sway/default.nix
@@ -96,15 +96,15 @@
             "${modifier}+7" = "workspace number 7";
             "${modifier}+8" = "workspace number 8";
             "${modifier}+9" = "workspace number 9";
-            "${modifier}+Shift+1" = "move container to workspace number 1";
-            "${modifier}+Shift+2" = "move container to workspace number 2";
-            "${modifier}+Shift+3" = "move container to workspace number 3";
-            "${modifier}+Shift+4" = "move container to workspace number 4";
-            "${modifier}+Shift+5" = "move container to workspace number 5";
-            "${modifier}+Shift+6" = "move container to workspace number 6";
-            "${modifier}+Shift+7" = "move container to workspace number 7";
-            "${modifier}+Shift+8" = "move container to workspace number 8";
-            "${modifier}+Shift+9" = "move container to workspace number 9";
+            "${modifier}+Shift+1" = "move container to workspace number 1; workspace number 1";
+            "${modifier}+Shift+2" = "move container to workspace number 2; workspace number 2";
+            "${modifier}+Shift+3" = "move container to workspace number 3; workspace number 3";
+            "${modifier}+Shift+4" = "move container to workspace number 4; workspace number 4";
+            "${modifier}+Shift+5" = "move container to workspace number 5; workspace number 5";
+            "${modifier}+Shift+6" = "move container to workspace number 6; workspace number 6";
+            "${modifier}+Shift+7" = "move container to workspace number 7; workspace number 7";
+            "${modifier}+Shift+8" = "move container to workspace number 8; workspace number 8";
+            "${modifier}+Shift+9" = "move container to workspace number 9; workspace number 9";
           };
         }; 
         # Extra config stuff that didn't have options in Nix
