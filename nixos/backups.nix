b7465447adc8ef7a53db427f186680e3ffce814b
techyporcupine <git@cb-tech.me>
2024-10-05 19:31:14 -0400

    prettier

--- nixos/backups.nix
+++ nixos/backups.nix
@@ -1,13 +1,21 @@
-{pkgs, config, lib, inputs, ... }: let cfg = config.tp.backups; in {
+{
+  pkgs,
+  config,
+  lib,
+  inputs,
+  ...
+}: let
+  cfg = config.tp.backups;
+in {
   options.tp.backups = {
     enable = lib.mkEnableOption "Enable Restic backups";
   };
 
   config = lib.mkIf cfg.enable {
-    restic = {
+    services.restic = {
       backups = {
-        remotebackup-small = {
-          passwordFile = ;
+        "remotebackup-small" = {
+          #passwordFile = ;
           initialize = true;
           extraOptions = [
             "sftp.command='ssh bowman4@100.64.0.6 -i /home/${config.tp.username}/.ssh/id_ed25519 -s sftp'"
@@ -27,10 +35,10 @@
     systemd = {
       timers = {
         "restic-remotebackup-small-remove" = {
-          wantedBy = [ "timers.target" ];
+          wantedBy = ["timers.target"];
           timerConfig = {
             OnCalendar = "daily";
-            Persistent = true; 
+            Persistent = true;
             Unit = "restic-remotebackup-small-remove.service";
           };
         };
@@ -38,7 +46,7 @@
       services = {
         "restic-remotebackup-small-remove" = {
           serviceConfig = {
-            ExecStart = ''/run/current-system/sw/bin/restic-remotebackup-small forget --keep-daily 7 --keep-weekly 26 --prune ''; 
+            ExecStart = "/run/current-system/sw/bin/restic-remotebackup-small forget --keep-daily 7 --keep-weekly 26 --prune";
             Type = "oneshot";
             User = "root";
           };
