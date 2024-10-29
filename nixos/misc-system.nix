cbc68c4a305eb3c8a209e67657ddcf434c200e8b
techyporcupine <git@cb-tech.me>
2024-10-29 11:04:14 -0400

    hypridle fixes

--- nixos/misc-system.nix
+++ nixos/misc-system.nix
@@ -36,6 +36,11 @@ in {
       ];
     };
 
+    services.logind.extraConfig = ''
+      HandlePowerKey=ignore
+      HandlePowerKeyLongPress=hibernate
+    '';
+
     # Enable HomeManager
     tp.hm.programs.home-manager.enable = true;
     tp.hm.systemd.user.startServices = "sd-switch";
