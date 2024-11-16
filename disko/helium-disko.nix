e4519424b00d75891b93743bdb6856ed046d881b
techyporcupine <git@cb-tech.me>
2024-11-16 15:00:45 -0500

    update disko conf

--- disko/helium-disko.nix
+++ disko/helium-disko.nix
@@ -1,56 +1,28 @@
 {
   disko.devices = {
     disk = {
-      # Set up disk called "vdb"
-      vdb = {
-        # TODO: CHANGEME
+      main = {
         device = "/dev/sda";
         type = "disk";
         content = {
           type = "gpt";
           partitions = {
             ESP = {
-              priority = 1;
-              name = "ESP";
-              start = "1M";
-              end = "512M";
               type = "EF00";
+              size = "512M";
               content = {
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
+                mountOptions = ["umask=0077"];
               };
             };
             root = {
               size = "100%";
               content = {
-                type = "btrfs";
-                extraArgs = ["-f"]; # Override existing partition
-                # BTRFS Subvolumes and where they are mounted
-                subvolumes = {
-                  # The rootfs, mounted at / on the disk
-                  "/rootfs" = {
-                    mountpoint = "/";
-                  };
-                  # the home dir, mounted at /home on the disk with some nice zstd compression
-                  "/home" = {
-                    mountOptions = ["compress=zstd"];
-                    mountpoint = "/home";
-                  };
-                  # the nix dir, mounted at /nix on the disk, also with some nice zstd compression
-                  "/nix" = {
-                    mountOptions = ["compress=zstd" "noatime"];
-                    mountpoint = "/nix";
-                  };
-                  # Subvolume for the swapfile, mounted at /.swapvol
-                  # 4GB should be good for 16GB of RAM w/o hibernation
-                  "/swap" = {
-                    mountpoint = "/.swapvol";
-                    swap = {
-                      swapfile.size = "4G";
-                    };
-                  };
-                };
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
               };
             };
           };
