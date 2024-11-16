58c9d5d88ca5fb63b1df11201dd47a3d7dd7f650
techyporcupine <git@cb-tech.me>
2024-11-15 21:00:12 -0500

    update and config backups

--- nixos/server/backups.nix
+++ nixos/server/backups.nix
@@ -14,75 +14,36 @@ in {
   config = lib.mkIf cfg.enable {
     services.restic = {
       backups = {
-        localbackup-small = {
-          passwordFile = /run/secrets/restic-password;
-          initialize = true;
-          paths = [
-            "/var/lib/vaultwarden/backups"
-            "/home/bowman4/dashy"
-            "/var/lib/hass/backups"
-            "/var/lib/unifi/data/backup/autobackup"
-            "/home/bowman4/.config/sops/age/"
-            "/srv/minecraft/broccoli-bloc/"
-          ];
-          repository = "/home/bowman4/resticbackups";
-          timerConfig = {
-            OnCalendar = "daily";
-            Persistent = true;
-            RandomizedDelaySec = "10min";
-          };
-        };
-        cloudbackup-small = {
-          passwordFile = config.sops.secrets.restic-password.path;
-          environmentFile = config.sops.secrets.restic-r2-env.path;
-          initialize = true;
-          paths = [
-            "/var/lib/vaultwarden/backups"
-            "/home/bowman4/dashy"
-            "/var/lib/hass/backups"
-            "/var/lib/unifi/data/backup/autobackup"
-            "/home/bowman4/.config/sops/age/"
-          ];
-          repository = "s3:https://6a8bb05e266bab6f4eee1fc6717f432a.r2.cloudflarestorage.com/tp-r2";
-          timerConfig = {
-            OnCalendar = "weekly";
-            Persistent = true;
-            RandomizedDelaySec = "10min";
-          };
-        };
-        remotebackup-small = {
-          passwordFile = config.sops.secrets.restic-password.path;
-          initialize = true;
-          extraOptions = [
-            "sftp.command='ssh bowman4@100.64.0.6 -i /home/bowman4/.ssh/id_ed25519 -s sftp'"
-          ];
-          paths = [
-            "/var/lib/vaultwarden/backups"
-            "/home/bowman4/dashy"
-            "/var/lib/hass/backups"
-            "/var/lib/unifi/data/backup/autobackup"
-            "/home/bowman4/.config/sops/age/"
-            "/srv/minecraft/broccoli-bloc/"
-          ];
-          repository = "sftp:bowman4@100.64.0.6:/mnt/PiBackup/ResticBackups/SmallBackup";
-          timerConfig = {
-            OnCalendar = "daily";
-            Persistent = true;
-            RandomizedDelaySec = "10min";
-          };
-        };
+        #localbackup-small = {
+        #  passwordFile = /run/secrets/restic-password;
+        #  initialize = true;
+        #  paths = [
+        #    "/var/lib/vaultwarden/backups"
+        #    "/home/bowman4/dashy"
+        #    "/var/lib/hass/backups"
+        #    "/var/lib/unifi/data/backup/autobackup"
+        #    "/srv/minecraft/broccoli-bloc/"
+        #  ];
+        #  repository = "/home/bowman4/resticbackups";
+        #  timerConfig = {
+        #    OnCalendar = "daily";
+        #    Persistent = true;
+        #    RandomizedDelaySec = "10min";
+        #  };
+        #};
         remotebackup-large = {
-          passwordFile = config.sops.secrets.restic-password.path;
+          passwordFile = /var/secrets/restic-password;
           initialize = true;
-          extraOptions = [
-            "sftp.command='ssh bowman4@100.64.0.6 -i /home/bowman4/.ssh/id_ed25519 -s sftp'"
-          ];
           paths = [
-            "/mnt/NixServeStorage/Data/Jellyfin"
+            "/var/lib/vaultwarden/backups"
+            "/home/beryllium/dashy"
+            "/var/lib/hass/"
+            "/var/lib/unifi/data/backup/autobackup"
+            "/srv/minecraft/broccoli-bloc/"
           ];
-          repository = "sftp:bowman4@100.64.0.6:/mnt/PiBackup/ResticBackups/LargeBackup";
+          repository = "rest:http://100.64.0.6:8000";
           timerConfig = {
-            OnCalendar = "weekly";
+            OnCalendar = "daily";
             Persistent = true;
             RandomizedDelaySec = "10min";
           };
@@ -92,30 +53,6 @@ in {
 
     systemd = {
       timers = {
-        "restic-localbackup-small-remove" = {
-          wantedBy = ["timers.target"];
-          timerConfig = {
-            OnCalendar = "daily";
-            Persistent = true;
-            Unit = "restic-localbackup-small-remove.service";
-          };
-        };
-        "restic-cloudbackup-small-remove" = {
-          wantedBy = ["timers.target"];
-          timerConfig = {
-            OnCalendar = "monthly";
-            Persistent = true;
-            Unit = "restic-cloudbackup-small-remove.service";
-          };
-        };
-        "restic-remotebackup-small-remove" = {
-          wantedBy = ["timers.target"];
-          timerConfig = {
-            OnCalendar = "daily";
-            Persistent = true;
-            Unit = "restic-remotebackup-small-remove.service";
-          };
-        };
         "restic-remotebackup-large-remove" = {
           wantedBy = ["timers.target"];
           timerConfig = {
@@ -126,30 +63,9 @@ in {
         };
       };
       services = {
-        "restic-localbackup-small-remove" = {
-          serviceConfig = {
-            ExecStart = ''/run/current-system/sw/bin/restic-localbackup-small forget --keep-daily 7 --keep-weekly 26 --prune '';
-            Type = "oneshot";
-            User = "root";
-          };
-        };
-        "restic-cloudbackup-small-remove" = {
-          serviceConfig = {
-            ExecStart = ''/run/current-system/sw/bin/restic-cloudbackup-small forget --keep-weekly 53 --prune '';
-            Type = "oneshot";
-            User = "root";
-          };
-        };
-        "restic-remotebackup-small-remove" = {
-          serviceConfig = {
-            ExecStart = ''/run/current-system/sw/bin/restic-remotebackup-small forget --keep-daily 7 --keep-weekly 26 --prune '';
-            Type = "oneshot";
-            User = "root";
-          };
-        };
         "restic-remotebackup-large-remove" = {
           serviceConfig = {
-            ExecStart = ''/run/current-system/sw/bin/restic-remotebackup-large forget --keep-weekly 26 --prune '';
+            ExecStart = ''/run/current-system/sw/bin/restic-remotebackup-large forget --keep-daily 2 --keep-weekly 26 --prune '';
             Type = "oneshot";
             User = "root";
           };
