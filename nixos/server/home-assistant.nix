ef8c9dd48907b12f71ba280f7ba3ab6dcc420b98
techyporcupine <git@cb-tech.me>
2024-12-28 14:19:39 -0500

    remove thread

--- nixos/server/home-assistant.nix
+++ nixos/server/home-assistant.nix
@@ -23,18 +23,6 @@ in {
           #"--device=/dev/ttyUSB0"
         ];
       };
-      openthread = {
-        volumes = ["/dev/ttyUSB1:/dev/ttyUSB1"];
-        image = "openthread/otbr"; # Warning: if the tag does not change, the image will not be updated
-        ports = ["127.0.0.1:8092:8080"];
-        cmd = ["--radio-url spinel+hdlc+uart:///dev/ttyUSB1"];
-        extraOptions = [
-          #"--sysctl ''net.ipv6.conf.all.disable_ipv6=0 net.ipv4.conf.all.forwarding=1 net.ipv6.conf.all.forwarding=1''"
-          "--dns=127.0.0.1"
-          "-it"
-          "--privileged"
-        ];
-      };
     };
     services.wyoming = {
       piper.servers.hasspiper = {
