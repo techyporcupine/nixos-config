810cfac9e366d913c5e8aa9e3f98ad1e91175351
techyporcupine <git@cb-tech.me>
2024-12-29 17:03:46 -0500

    disable systemdboot

--- machines/helium.nix
+++ machines/helium.nix
@@ -49,6 +49,7 @@
   ];
 
   # Use the extlinux boot loader. (NixOS wants to enable GRUB by default)
+  boot.loader.systemd-boot.enable = lib.mkForce false;
   boot.loader.grub.enable = lib.mkForce false;
   # Enables the generation of /boot/extlinux/extlinux.conf
   boot.loader.generic-extlinux-compatible.enable = lib.mkForce true;
