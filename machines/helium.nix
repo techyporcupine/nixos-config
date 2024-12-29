68ebe59163dcaa4c5de33a7bd5d8d6d49e3890cb
techyporcupine <git@cb-tech.me>
2024-12-29 17:01:11 -0500

    force changes to bootloader

--- machines/helium.nix
+++ machines/helium.nix
@@ -49,9 +49,9 @@
   ];
 
   # Use the extlinux boot loader. (NixOS wants to enable GRUB by default)
-  boot.loader.grub.enable = false;
+  boot.loader.grub.enable = lib.mkForce false;
   # Enables the generation of /boot/extlinux/extlinux.conf
-  boot.loader.generic-extlinux-compatible.enable = true;
+  boot.loader.generic-extlinux-compatible.enable = lib.mkForce true;
 
   # Mount 1TB Hard drive
   #fileSystems."/mnt/HDD" = {
