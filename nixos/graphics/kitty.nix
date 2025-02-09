87ef97ae429a14f64ccb69829f6d712599d10e04
techyporcupine <git@cb-tech.me>
2025-02-09 16:55:24 -0500

    add keybinds for switching tabs

--- nixos/graphics/kitty.nix
+++ nixos/graphics/kitty.nix
@@ -18,6 +18,18 @@ in {
         name = "Fira-Code";
         size = 11;
       };
+      keybindings = {
+        "alt+1" = "goto_tab 1";
+        "alt+2" = "goto_tab 2";
+        "alt+3" = "goto_tab 3";
+        "alt+4" = "goto_tab 4";
+        "alt+5" = "goto_tab 5";
+        "alt+6" = "goto_tab 6";
+        "alt+7" = "goto_tab 7";
+        "alt+8" = "goto_tab 8";
+        "alt+9" = "goto_tab 9";
+        "alt+10" = "goto_tab 10";
+      };
       themeFile = "Catppuccin-Mocha";
       shellIntegration.enableZshIntegration = true;
     };
