7e754e3ee0ec0a6024b58faeadb5c1a1e5176004
techyporcupine <git@cb-tech.me>
2025-03-24 12:38:50 -0400

    change format again

--- nixos/server/matrix.nix
+++ nixos/server/matrix.nix
@@ -14,22 +14,24 @@ in {
   config = lib.mkIf cfg.enable {
     services.matrix-synapse = {
       enable = true;
-      server_name = "matrix.cb-tech.me";
-      settings.listeners = [
-        {
-          port = 8118;
-          bind_addresses = ["::1"];
-          type = "http";
-          tls = false;
-          x_forwarded = true;
-          resources = [
-            {
-              names = ["client" "federation"];
-              compress = false;
-            }
-          ];
-        }
-      ];
+      settings = {
+        server_name = "matrix.cb-tech.me";
+        listeners = [
+          {
+            port = 8118;
+            bind_addresses = ["::1"];
+            type = "http";
+            tls = false;
+            x_forwarded = true;
+            resources = [
+              {
+                names = ["client" "federation"];
+                compress = false;
+              }
+            ];
+          }
+        ];
+      };
     };
     services.traefik.dynamicConfigOptions.http = {
       routers = {
