b033e31731e13ca2c75e0ecd14ec9cc90d2e473d
techyporcupine <git@cb-tech.me>
2025-03-30 16:49:06 -0400

    change mongo version

--- nixos/server/unifi/default.nix
+++ nixos/server/unifi/default.nix
@@ -18,7 +18,7 @@ in {
       openFirewall = true;
       # use latest unifi package
       unifiPackage = pkgs.unifi;
-      mongodbPackage = pkgs.mongodb-ce;
+      mongodbPackage = pkgs.mongodb;
     };
 
     nixpkgs.overlays = [
