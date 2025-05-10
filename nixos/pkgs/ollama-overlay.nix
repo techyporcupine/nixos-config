ecd9f33e96a7ba75fbc8a8447b34801c0222566f
techyporcupine <git@cb-tech.me>
2025-05-10 10:58:04 -0400

    force mmq

--- nixos/pkgs/ollama-overlay.nix
+++ nixos/pkgs/ollama-overlay.nix
@@ -1,5 +1,10 @@
 self: super: {
   ollama = super.ollama.overrideAttrs (old: {
-    cmakeFlags = (old.cmakeFlags or []) ++ ["-DCMAKE_CUDA_ARCHITECTURES=50;52"];
+    cmakeFlags =
+      (old.cmakeFlags or [])
+      ++ [
+        "-DCMAKE_CUDA_ARCHITECTURES=50;52"
+        "-DLLAMA_CUDA_FORCE_MMQ=on"
+      ];
   });
 }
