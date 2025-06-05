25fa660201b09020bdc31cc2ddf3f39ddfabc9da
techyporcupine <git@cb-tech.me>
2025-06-05 17:46:39 -0400

    different mongo

--- nixos/server/unifi/default.nix
+++ nixos/server/unifi/default.nix
@@ -18,7 +18,7 @@ in {
       openFirewall = true;
       # use latest unifi package
       unifiPackage = pkgs.unifi;
-      mongodbPackage = pkgs.stable.mongodb;
+      mongodbPackage = pkgs.stable.mongodb-ce;
     };
 
     nixpkgs.overlays = [
