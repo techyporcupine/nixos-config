bea8e3928d18d33d4a15e331ba3db4cdeeb0f7d6
techyporcupine <git@cb-tech.me>
2025-11-03 12:08:05 -0500

    remove flatpak

--- machines/carbon.nix
+++ machines/carbon.nix
@@ -150,8 +150,6 @@
 
   boot.binfmt.emulatedSystems = ["aarch64-linux"];
 
-  services.flatpak.enable = true;
-
   # FIXME: When installing this flake, comment out the following 5 lines until you have rebooted into the new system and decide you want secure boot!
   # boot.loader.systemd-boot.enable = lib.mkForce false;
   # boot.lanzaboote = {
