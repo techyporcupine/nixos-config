af7b5c3b83ade871994a37ce0bd4643694918534
techyporcupine <git@cb-tech.me>
2025-11-05 22:13:04 -0500

    update vaapi def

--- machines/beryllium.nix
+++ machines/beryllium.nix
@@ -16,7 +16,7 @@
         "openssl-1.1.1w"
       ];
       packageOverrides = pkgs: {
-        vaapiIntel = pkgs.vaapiIntel.override {enableHybridCodec = true;};
+        intel-vaapi-driver = pkgs.intel-vaapi-driver.override {enableHybridCodec = true;};
       };
     };
   };
