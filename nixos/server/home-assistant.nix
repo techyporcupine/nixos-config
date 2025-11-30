f15ce1818ae4776e8dbb20b47d7c5f950a2b8c46
techyporcupine <git@cb-tech.me>
2025-11-30 17:00:06 -0500

    enable HA

--- nixos/server/home-assistant.nix
+++ nixos/server/home-assistant.nix
@@ -52,7 +52,7 @@ in {
     services.zigbee2mqtt = {
       enable = true;
       settings = {
-        # homeassistant = true;
+        homeassistant = true;
         serial = {
           port = "/dev/serial/by-id/usb-Itead_Sonoff_Zigbee_3.0_USB_Dongle_Plus_V2_24d5aba9cb12ef1183936db8bf9df066-if00-port0";
           adapter = "ember";
