5886355335ac23a05944be374304ae583fb0c5f4
Jonathan Bowman <git@bowmanjd.com>
2025-12-17 05:35:17 -0500

    fix: ollama package

--- machines/boron.nix
+++ machines/boron.nix
@@ -85,7 +85,7 @@
     enable = true;
     host = "0.0.0.0";
     openFirewall = true;
-		package = "ollama-cuda";
+		package = pkgs.ollama-cuda;
   };
 
   # Virtualisation settings: podman and OCI container entries (open-webui)
