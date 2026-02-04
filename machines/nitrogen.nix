ca2d6c12ca85494903959183d1d4d0a945dd33f9
techyporcupine <git@cb-tech.me>
2026-02-03 20:28:42 -0500

    disable

--- machines/nitrogen.nix
+++ machines/nitrogen.nix
@@ -71,7 +71,7 @@
 
   # Machine-specific packages
   environment.systemPackages = with pkgs; [
-    llama-cpp-cuda-native-llguidance
+    #llama-cpp-cuda-native-llguidance
   ];
 
   # Local overlays (adds project-specific package overlays)
