1377077b5b1134361bc66f4d1052e58500b37d1d
techyporcupine <git@cb-tech.me>
2026-04-27 19:11:13 -0400

    clean up

--- machines/beryllium.nix
+++ machines/beryllium.nix
@@ -102,36 +102,6 @@
 
       # usbhid-ups driver always use value "auto"
       port = "auto";
-
-      directives = [
-        # "Restore power on AC" BIOS option needs power to be cut a few seconds to work;
-        # this is achieved by the offdelay and ondelay directives.
-
-        # in the last stages of system shutdown, "upsdrvctl shutdown" is called to tell UPS that
-        # after offdelay seconds, the UPS power must be cut, even if
-        # wall power returns.
-
-        # There is a danger that the system will take longer than the default 20 seconds to shut down.
-        # If that were to happen, the UPS shutdown would provoke a brutal system crash.
-        # We adjust offdelay, to solve this issue.
-        "offdelay = 120"
-
-        # UPS power is now cut regardless of wall power.  After (ondelay minus offdelay) seconds,
-        # if wall power returns, turn on UPS power.  The system has now been disconnected for a minimum of (ondelay minus offdelay) seconds,
-        # "Restore power on AC" should now power on the system.
-        # For reasons described above, ondelay value must be larger than offdelay value.
-        # We adjust ondelay, to ensure Restore power on AC option returns to Power Disconnected state.
-        "ondelay = 130"
-
-        # set value for battery.charge.low,
-        # upsmon initiate shutdown once this threshold is reached.
-        "lowbatt = 20"
-
-        # ignore it if the UPS reports a low battery condition
-        # without this, system will shutdown only when ups reports lb,
-        # not respecting lowbatt option
-        "ignorelb"
-      ];
     };
 
     upsd = {
