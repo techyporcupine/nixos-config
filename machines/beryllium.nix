60b16f3d152c670d6fca5ca5b556cacd6928a312
techyporcupine <git@cb-tech.me>
2026-04-29 11:42:23 -0400

    change powervalue

--- machines/beryllium.nix
+++ machines/beryllium.nix
@@ -134,7 +134,7 @@
     # section: The upsmon daemon configuration: upsmon.conf
     upsmon.monitor."smt1500" = {
       system = "smt1500@localhost";
-      powerValue = 1;
+      powerValue = 0;
       user = "admin";
       passwordFile = "/var/secrets/ups-passwd";
       type = "primary";
