206115d2a861c6c34690ceede4034cb534b1b825
techyporcupine <git@cb-tech.me>
2026-06-05 10:49:08 -0400

    change wyoming to nitrogen kokoro

--- machines/boron.nix
+++ machines/boron.nix
@@ -137,7 +137,7 @@
             WYOMING_URI = "tcp://0.0.0.0:10300";
             WYOMING_LOG_LEVEL = "INFO";
             WYOMING_LANGUAGES = "en";
-            TTS_OPENAI_URL = "http://10.0.0.10:8880/v1";
+            TTS_OPENAI_URL = "http://10.0.0.11:8880/v1";
             TTS_STREAMING_MODELS = "kokoro";
             TTS_MODELS = "kokoro";
             TTS_BACKEND = "KOKORO_FASTAPI";
